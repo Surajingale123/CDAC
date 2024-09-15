65ade4f1d371c99e4b627963a3eb8b417df89503
Suraj Ingale <80613400+Surajingale123@users.noreply.github.com>
2024-09-15 23:57:08 +0530

    Add files via upload

---
+++ JavaOOPS/Assignments/Sir Assignment 3/Assignment4/Question2/Program.java
@@ -0,0 +1,16 @@
+package org.program;
+
+import org.CompoundInterestCalculatorUtil.CompoundInterestCalculatorUtil;
+
+public class Program {
+
+	public static void main(String[] args) {
+		CompoundInterestCalculatorUtil lol=new CompoundInterestCalculatorUtil();
+
+		lol.acceptRecord();
+		lol.futureValueCalculation();
+		lol.totalInteret();
+		lol.showRecord();
+	}
+
+}
